7e3eab6e380f159b4893db5d3def1e77109b5242
estebanCastaneira <estebanc_b@hotmail.com>
2025-01-04 13:52:19 -0300

    Update Home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,32 @@
 function Home() {
   return (
-    <div className="flex justify-center">
-      <section className="container">
-        <h2 className="">Esto es la home</h2>
-      </section>
+    <div className="flex justify-center text-white text-center">
+      <div className="container my-14">
+        <section className="backdrop-blur-[20px] backdrop-saturate-[92%] bg-[#ffffff] bg-opacity-30 border border-opacity-20 border-white w-[80%]  my-0 mx-auto p-8 rounded-lg tracking-wider flex flex-col gap-10">
+          <h1 className="text-3xl">
+            Welcome to the World Travel, Inc. Technical Interview Front End!!!
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit
+            molestias aspernatur, quam repellat sequi rerum, totam quis magnam
+            suscipit doloremque non excepturi quaerat impedit in voluptatem
+            minima, cupiditate dolorum omnis!
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit
+            molestias aspernatur, quam repellat sequi rerum, totam quis magnam
+            suscipit doloremque non excepturi quaerat impedit in voluptatem
+            minima, cupiditate dolorum omnis!
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit
+            molestias aspernatur, quam repellat sequi rerum, totam quis magnam
+            suscipit doloremque non excepturi quaerat impedit in voluptatem
+            minima, cupiditate dolorum omnis!
+          </p>
+          {/* TODO */}
+        </section>
+      </div>
     </div>
   )
 }
