738ea35d78c6ffb3e4611905df78c72d5f3872b5
estebanCastaneira <estebanc_b@hotmail.com>
2025-01-04 14:43:16 -0300

    Add initial configuration for react-redux and react-persist

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,11 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import { BrowserRouter } from "react-router-dom"
-import "./index.css"
+// import { Provider } from "react-redux"
+// import { PersistGate } from "redux-persist/integration/react"
+// import { store, persistor } from "./redux_config/store.js"
 import App from "./App.jsx"
+import "./index.css"
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
