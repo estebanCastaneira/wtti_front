f3ad93520e79520ca7ece082905d4a95aa7a8a34
estebanCastaneira <estebanc_b@hotmail.com>
2025-01-04 13:56:35 -0300

    Styles Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,7 @@ function Home() {
   return (
     <div className="flex justify-center text-white text-center">
       <div className="container my-14">
-        <section className="backdrop-blur-[20px] backdrop-saturate-[92%] bg-[#ffffff] bg-opacity-30 border border-opacity-20 border-white w-[80%]  my-0 mx-auto p-8 rounded-lg tracking-wider flex flex-col gap-10">
+        <section className="backdrop-blur-[20px] backdrop-saturate-[92%] bg-[#ffffff] bg-opacity-30 border border-opacity-20 border-white w-[90%] my-0 mx-auto p-16 lg:py-16 lg:px-56 rounded-lg tracking-wider flex flex-col gap-10">
           <h1 className="text-3xl">
             Welcome to the World Travel, Inc. Technical Interview Front End!!!
           </h1>
