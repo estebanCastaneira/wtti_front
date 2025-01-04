6e14cd53ff588b1b9eca693bd34bd8628f6d991d
estebanCastaneira <estebanc_b@hotmail.com>
2025-01-04 16:16:00 -0300

    Fix links hrefs

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,17 +7,13 @@ function Header() {
     <header className="text-white tracking-widest bg-blue-900 flex justify-center p-5 ">
       <nav className="container">
         <ul className="flex gap-10">
-          <Anchor currentPage={currentPage} href={"/home"} pageName={"Home"} />
+          <Anchor currentPage={currentPage} href={"/"} pageName={"Home"} />
           <Anchor
             currentPage={currentPage}
-            href={"/external"}
+            href={"external"}
             pageName={"External"}
           />
-          <Anchor
-            currentPage={currentPage}
-            href={"/local"}
-            pageName={"Local"}
-          />
+          <Anchor currentPage={currentPage} href={"local"} pageName={"Local"} />
         </ul>
       </nav>
     </header>
