b328ce3aa62b94b56039015741da0b2839c4a9df
estebanCastaneira <estebanc_b@hotmail.com>
2025-01-04 18:14:52 -0300

    Update styles to Home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,8 @@
 function Home() {
   return (
     <div className="flex justify-center text-white text-center">
-      <div className="container my-14">
-        <section className="backdrop-blur-[20px] backdrop-saturate-[92%] bg-[#ffffff] bg-opacity-30 border border-opacity-20 border-white w-[90%] my-0 mx-auto p-16 lg:py-16 lg:px-56 rounded-lg tracking-wider flex flex-col gap-10">
+      <div className="container my-10">
+        <section className="backdrop-blur-[20px] backdrop-saturate-[92%] bg-[#ffffff] bg-opacity-30 border border-opacity-20 border-white w-[90%] my-0 mx-auto p-16 lg:py-16 lg:px-56 rounded-lg flex flex-col gap-10">
           <h1 className="text-3xl">
             Welcome to the World Travel, Inc. Technical Interview Front End!!!
           </h1>
