04ffdc7f806738783b2b4393c225704fcc5f4359
estebanCastaneira <estebanc_b@hotmail.com>
2025-01-04 22:56:21 -0300

    Add store data logic to component LocalTable

--- src/components/LocalTable.jsx
+++ src/components/LocalTable.jsx
@@ -1,15 +1,18 @@
 import Table from "./Table"
+import { useSelector } from "react-redux"
 
 function LocalTable() {
+  const localData = useSelector((state) => state.localData)
+  console.log(localData)
   return (
     <div className="flex justify-center">
       <div className="container my-10">
         <div className="my-6">
-          <button className="text-white bg-blue-900 border border-blue-300 p-2 rounded-2xl hover:bg-purple-500 relative active:top-1 active:left-1">
+          <button className=" bg-blue-900 border border-blue-300 p-2 rounded-2xl hover:bg-purple-500 relative active:top-1 active:left-1">
             Create
           </button>
         </div>
-        <Table />
+        <Table data={localData} />
       </div>
     </div>
   )
