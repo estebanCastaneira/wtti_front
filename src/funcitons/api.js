fef89afac71acc2b8c2878c3ba34156e766266f6
estebanCastaneira <estebanc_b@hotmail.com>
2025-01-06 12:26:48 -0300

    Add updateBook function as axios call

--- src/funcitons/api.js
+++ src/funcitons/api.js
@@ -9,3 +9,16 @@ export const getBooks = async () => {
     throw error
   }
 }
+
+export const updateBook = async (book) => {
+  try {
+    const response = await axios.put(
+      `${import.meta.env.VITE_BASE_URL}/${book._version_}`,
+      book
+    )
+    return response.data // Devuelve la respuesta del backend si es necesario
+  } catch (error) {
+    console.error("Error updating book:", error)
+    throw error
+  }
+}
